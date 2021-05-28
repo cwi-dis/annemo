d95fd9f5da48e18814f7bed139555a10a6c8e1bb
Thomas Röggla <tom@lichtfragmente.com>
2021-05-28 20:19:45 +0200

    submitting data to server when slider changes

--- static/js/components/video.tsx
+++ static/js/components/video.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { RouterParams } from "./app";
 
@@ -7,8 +7,11 @@ interface VideoProps {
 }
 
 const Video: React.FC<VideoProps> = (props) => {
-  const { video, dimension } = useParams<RouterParams>();
+  const { video, dimension, subject } = useParams<RouterParams>();
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   const [ location, setLocation ] = useState<string>();
+  const [ sliderValue, setSliderValue ] = useState<number>(0);
 
   useEffect(() => {
     fetch("/location").then((res) => {
@@ -17,6 +20,32 @@ const Video: React.FC<VideoProps> = (props) => {
       setLocation(data.location);
     });
   }, []);
+
+  const onSliderChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (videoRef.current) {
+      const videoElement = videoRef.current;
+
+      const data = {
+        clienttime: Date.now(),
+        subject,
+        video,
+        dimension,
+        time: videoElement.currentTime,
+        value: e.target.valueAsNumber,
+        playing: !videoElement.paused,
+      };
+
+      fetch("/emotion", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          subject, data
+        })
+      });
+    }
+
+    setSliderValue(e.target.valueAsNumber);
+  };
 
   return (
     <div style={{ width: "75%" }}>
@@ -30,11 +59,21 @@ const Video: React.FC<VideoProps> = (props) => {
               width="800"
               height="600"
               preload="auto"
+              ref={videoRef}
               controls
             />
 
             <br/>
-            <input type="range" style={{ width: 800 }} />
+            <br/>
+            <input
+              type="range"
+              value={sliderValue}
+              min={-1}
+              max={1}
+              step={0.01}
+              style={{ width: 800 }}
+              onChange={onSliderChanged}
+            />
           </div>
         )}
       </div>
