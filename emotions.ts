654189189e1bba6b8b81c5f3e7d47f461db2b50d
Thomas Röggla <tom@lichtfragmente.com>
2021-05-28 19:44:58 +0200

    added POST routes for saving data to csv files

--- emotions.ts
+++ emotions.ts
@@ -2,8 +2,33 @@ import express from "express";
 import path from "path";
 import fs from "fs";
 import logger from "morgan";
+import dateFormat from "dateformat";
 
 import config from "./config.json";
+
+function escapeQuotes(s: string) {
+  if (s.indexOf(",") >= 0) {
+    return '"' + s.replace(/"/g, '\\"') + '"';
+  }
+
+  return s;
+}
+
+async function saveToCSV(subject: string, line: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const writer = fs.createWriteStream(path.join(__dirname, "results", `${subject}.csv`), {
+      flags: "a"
+    });
+
+    writer.write(line, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
 const app = express();
 const resultsDir = path.join(__dirname, "results");
@@ -32,6 +57,45 @@ app.get("/videos", (req, res) => {
   res.send({
     videos: config.videos
   });
+});
+
+app.post("/emotion", async (req, res) => {
+  const { subject, data } = req.body;
+
+  const values = Object.values(data).map(escapeQuotes);
+  const formattedDate = escapeQuotes(dateFormat(Date.now(), "dddd, mmmm dS, yyyy, h:MM:ss TT"))
+
+  try {
+    const csv = formattedDate + "," + values.join(",") + "\n"
+    await saveToCSV(subject, csv);
+
+    res.send({
+      status: "OK"
+    })
+  } catch (err) {
+    res.status(500).send({
+      message: err
+    });
+  }
+});
+
+app.post("/social", async (req, res) => {
+  const { subject, data } = req.body;
+
+  const values = Object.values(data).map(escapeQuotes);
+
+  try {
+    const csv = "Social," + values.join(",") + "\n";
+    await saveToCSV(subject, csv);
+
+    res.send({
+      status: "OK"
+    })
+  } catch (err) {
+    res.status(500).send({
+      message: err
+    });
+  }
 });
 
 app.get("/location", (req, res) => {
