ee3cf6a9b17579b8f903f2daa1baafc4a6d75d9e
Thomas Röggla <tom@lichtfragmente.com>
2021-05-28 22:21:00 +0200

    loading config and then starting server

--- app.ts
+++ app.ts
@@ -55,99 +55,109 @@ async function saveToCSV(subject: string, line: string): Promise<void> {
   });
 }
 
-const app = express();
-const resultsDir = path.join(__dirname, "results");
+function startServer(config: Config) {
+  const app = express();
+  const resultsDir = path.join(__dirname, "results");
 
-if (!fs.existsSync(resultsDir)) {
-  fs.mkdirSync(resultsDir, 0o755);
+  if (!fs.existsSync(resultsDir)) {
+    fs.mkdirSync(resultsDir, 0o755);
+  }
+
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "ejs");
+
+  const port = process.env.PORT || 3001;
+  app.set("port", port);
+
+  app.use(express.static("static"))
+  app.use(express.urlencoded({ extended: false }));
+  app.use(logger("dev"));
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.render("index", {
+      title: "Annemo"
+    });
+  });
+
+  app.get("/videos", (req, res) => {
+    const { subject } = req.query;
+
+    if (config.users.find((u) => u == subject)) {
+      return res.send({
+        videos: config.videos
+      });
+    }
+
+    res.send({ videos: [] })
+  });
+
+  app.post("/emotion", async (req, res) => {
+    const { subject, data } = req.body;
+
+    if (!config.users.find((u) => u == subject)) {
+      return res.status(400).send({
+        message: "No such user"
+      });
+    }
+
+    const values = Object.values(data).map(escapeQuotes);
+    const formattedDate = escapeQuotes(dateFormat(Date.now(), "dddd, mmmm dS, yyyy, h:MM:ss TT"))
+
+    try {
+      const csv = formattedDate + "," + values.join(",") + "\n"
+      await saveToCSV(subject, csv);
+
+      res.send({
+        status: "OK"
+      })
+    } catch (err) {
+      res.status(500).send({
+        message: err
+      });
+    }
+  });
+
+  app.post("/social", async (req, res) => {
+    const { subject, data } = req.body;
+
+    if (!config.users.find((u) => u == subject)) {
+      return res.status(400).send({
+        message: "No such user"
+      });
+    }
+
+    const values = Object.values(data).map(escapeQuotes);
+
+    try {
+      const csv = "Social," + values.join(",") + "\n";
+      await saveToCSV(subject, csv);
+
+      res.send({
+        status: "OK"
+      })
+    } catch (err) {
+      res.status(500).send({
+        message: err
+      });
+    }
+  });
+
+  app.get("/location", (req, res) => {
+    res.send({
+      location: config.location
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server listening on port http://0.0.0.0/${port}`);
+  });
 }
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-const port = process.env.PORT || 3001;
-app.set("port", port);
-
-app.use(express.static("static"))
-app.use(express.urlencoded({ extended: false }));
-app.use(logger("dev"));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Annemo"
-  });
-});
-
-app.get("/videos", (req, res) => {
-  const { subject } = req.query;
-
-  if (config.users.find((u) => u == subject)) {
-    return res.send({
-      videos: config.videos
-    });
-  }
-
-  res.send({ videos: [] })
-});
-
-app.post("/emotion", async (req, res) => {
-  const { subject, data } = req.body;
-
-  if (!config.users.find((u) => u == subject)) {
-    return res.status(400).send({
-      message: "No such user"
-    });
-  }
-
-  const values = Object.values(data).map(escapeQuotes);
-  const formattedDate = escapeQuotes(dateFormat(Date.now(), "dddd, mmmm dS, yyyy, h:MM:ss TT"))
-
-  try {
-    const csv = formattedDate + "," + values.join(",") + "\n"
-    await saveToCSV(subject, csv);
-
-    res.send({
-      status: "OK"
-    })
-  } catch (err) {
-    res.status(500).send({
-      message: err
-    });
-  }
-});
-
-app.post("/social", async (req, res) => {
-  const { subject, data } = req.body;
-
-  if (!config.users.find((u) => u == subject)) {
-    return res.status(400).send({
-      message: "No such user"
-    });
-  }
-
-  const values = Object.values(data).map(escapeQuotes);
-
-  try {
-    const csv = "Social," + values.join(",") + "\n";
-    await saveToCSV(subject, csv);
-
-    res.send({
-      status: "OK"
-    })
-  } catch (err) {
-    res.status(500).send({
-      message: err
-    });
-  }
-});
-
-app.get("/location", (req, res) => {
-  res.send({
-    location: config.location
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port http://0.0.0.0/${port}`);
+loadConfig().then((config) => {
+  console.log("Config:", config);
+  startServer(config);
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
