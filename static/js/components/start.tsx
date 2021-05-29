138d776c6ed71e0846f78fae410328e069d0bdf4
Thomas Röggla <tom@lichtfragmente.com>
2021-05-29 16:27:31 +0200

    replaced width property with column specification in start component

--- static/js/components/start.tsx
+++ static/js/components/start.tsx
@@ -5,7 +5,7 @@ interface StartProps {
 
 const Start: React.FC<StartProps> = (props) => {
   return (
-    <div style={{ width: "75%" }}>
+    <div className="column is-9">
       <h5 className="title is-5 has-text-centered mt-4">
         Select a video on the left to get started
       </h5>
