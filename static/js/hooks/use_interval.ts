cd532f2bf33d70557a53e391a45e0cfd4d434122
Thomas Röggla <t.roggla@cwi.nl>
2021-07-30 15:11:12 +0200

    added documentation for useInterval()

--- static/js/hooks/use_interval.ts
+++ static/js/hooks/use_interval.ts
@@ -1,18 +1,32 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * Allows for the use of `setInterval()` as a React hook. Works analogous to
+ * `setInterval()` with the exception that if `delay` is set to `null`, the
+ * interval is cleared.
+ *
+ * @param callback Callback function to be called repeatedly
+ * @param delay Delay between invocations of `callback` in ms
+ */
 function useInterval(callback: () => void, delay: number | null) {
+  // Ref to callback function
   const currentCallback = useRef<() => void>();
 
   useEffect(() => {
+    // Update callback ref everytime `callback` changes
     currentCallback.current = callback;
   }, [callback]);
 
+  // Set up the interval every time `delay` changes
   useEffect(() => {
+    // Only start interval if `delay` is not `null`
     if (delay) {
+      // Start interval and invoke the function in `currentCallback`
       const interval = setInterval(() => {
         currentCallback.current?.();
       }, delay);
 
+      // Clear the interval on unmount
       return () => {
         clearInterval(interval);
       };
